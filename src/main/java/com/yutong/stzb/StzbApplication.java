fc5bf074d99445637cbb5ea56157c5cbd68da21a
GakkiBest <13783944207@163.com>
2024-05-29 21:34:34 +0800

    feat ： 初始化项目

---
+++ src/main/java/com/yutong/stzb/StzbApplication.java
@@ -0,0 +1,13 @@
+package com.yutong.stzb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StzbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StzbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yutong/stzb/StzbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yutong.stzb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StzbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
